0e8c9d08b61585ee250ee5a841d3996ff34be7c9
Derek Anderson <derek@armyofevilrobots.com>
2012-09-23 10:31:56 -0700

    Fix list

--- reticulatus/geo/stl.py
+++ reticulatus/geo/stl.py
@@ -366,49 +366,19 @@ class STL():
             print "  P3:     %f %f %f" % (p3[0], p3[1], p3[2])
             print "  Attr:   0x%X" % a
 
-    def to_cgal_poly(self):
-        """
-        Takes this giant pile of edges/faces and generates CGAL polys.
-        """
-        from CGAL.CGAL_Kernel import Point_3
-        from CGAL.CGAL_Polyhedron_3 import Polyhedron_3
-        import sys
-        poly = Polyhedron_3()
-        for facet in self._facets:
-            points = [Point_3(*point) for point in facet['p']]
-            #print points
-            #print (poly.make_triangle.__doc__)
-            if len(points) == 3:
-                poly.make_triangle(*points)
-        return poly
-
-
-    #def generate_planar_intersections(self, start_height, inc_height, max_height):
-        #"""Generates a series of slices from height start_height and then
-        #every additional inc_height units."""
+    #def to_cgal_poly(self):
+        #"""
+        #Takes this giant pile of edges/faces and generates CGAL polys.
+        #"""
         #from CGAL.CGAL_Kernel import Point_3
-        #from CGAL.CGAL_Kernel import Vector_3
-        #from CGAL.CGAL_Kernel import Plane_3
         #from CGAL.CGAL_Polyhedron_3 import Polyhedron_3
-        #from CGAL.CGAL_AABB_tree import AABB_tree_Polyhedron_3_Facet_handle
-
-        #poly = self.to_cgal_poly()
-
-        ##This is a CGAL tree that makes for fast slicening!
-        #tree = AABB_tree_Polyhedron_3_Facet_handle(poly.facets())
-        #zpos = start_height
-        ## This should _really_ be the max height from a bounding box eh?
-        #layers = list()
-        #while zpos < max_height:
-            #vec = Vector_3(0, 0, 1)
-            #plane_query = Plane_3(Point_3(0, 0, zpos), vec)
-            #intersections = list()
-            #tree.all_intersections(plane_query, intersections)
-            #layers.append((zpos, intersections))
-            #zpos += inc_height
-        #return layers
-
-
+        #import sys
+        #poly = Polyhedron_3()
+        #for facet in self._facets:
+            #points = [Point_3(*point) for point in facet['p']]
+            #if len(points) == 3:
+                #poly.make_triangle(*points)
+        #return poly
 
 
 
